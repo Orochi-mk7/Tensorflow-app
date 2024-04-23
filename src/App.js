e415802678121533187a3525223901e5b3405466
Orochi-mk7 <oroc.oroc.mohamad@gmail.com>
2024-04-23 18:41:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
+import React, {useRef, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as tf from "@tensorflow/tfjs";
+import * as qna from "@tensorflow-models/qna";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
+import { Fragment } from 'react';
 
-function App() {
+
+const App = () => {
+
+  const passageRef = useRef(null); 
+  const questionRef = useRef(null);
+  const [answer, setAnswer] = useState(); 
+  const [model, setModel] = useState(null); 
+
+  const loadModel = async ()=>{
+    const loadedModel = await qna.load()
+    setModel(loadedModel); 
+    console.log('Model loaded.')
+  } 
+
+  const answerQuestion = async (e) =>{
+    if (e.which === 13 && model !== null ){
+      console.log('Question submitted.')
+      const passage = passageRef.current.value
+      const question = questionRef.current.value
+
+      const answers = await model.findAnswers(question, passage)
+      setAnswer(answers); 
+      console.log(answers)
+
+    }  
+  }
+
+  useEffect(()=>{loadModel()}, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {model ==null ? 
+          <div>
+            <div>Model Loading</div>      
+            <Loader
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}/>
+          </div> 
+          :  
+          <React.Fragment>
+            Passage
+            <textarea ref={passageRef} rows="30" cols="100"></textarea>
+            Ask a Question
+            <input ref={questionRef} onKeyDown={answerQuestion} size="80"></input>
+            <br /> 
+            Answers
+            {answer ? answer.map((ans, idx) =><div><b>Answer {idx+1} - </b> {ans.text} ({Math.floor(ans.score*100)/100})</div>) : ""}
+            </React.Fragment>
+        } 
       </header>
     </div>
   );
